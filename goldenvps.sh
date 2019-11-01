b0de9eb63205e68342035c9314475ae8d799ee48
nfreeargentina <57245522+nfreeargentina@users.noreply.github.com>
2019-11-01 01:28:27 -0300

    Update goldenvps.sh

--- goldenvps.sh
+++ goldenvps.sh
@@ -142,7 +142,7 @@ chmod +x ${ARQ}/$1
 fun_ip
 wget -O /usr/bin/trans https://raw.githubusercontent.com/GoldenADMPRO/GOLDEN-ADM-MANAGER/master/Install/trans &> /dev/null
 msg -bar2
-msg -ama "[ GOLDEN ADM PRO SCRIPT]"
+msg -ama "[ GOLDEN ADM ARGENTINA]"
 [[ $1 = "" ]] && funcao_idioma || {
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
  }
